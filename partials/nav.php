98bd0ce8295d74490775601c34a4fd1b57dcdeb5
Shashwat <harshshashwat18@gmail.com>
2021-08-25 22:37:16 +0530

    Updating nav-bar values

--- partials/nav.php
+++ partials/nav.php
@@ -6,15 +6,15 @@
 <nav id='navbar'>
     <div class="nav-container"> 
         <ul class="nav-list">
-            <li class="element"><a href="">Home</a></li>
+            <li class="element"><a href="/programs/Php_Projects/Login_System">Home</a></li>
             <li class="element"><a href="">Events</a></li>
             <li class="element"><a href="">Services</a></li>
             <li class="element"><a href="">Contact us</a></li>
             <li class="element"><a href="">About</a></li>
         </ul>
         <ul class="nav-list log">
-            <li class="element"><a href="">Login</a></li>
-            <li class="element"><a href="">Logout</a></li>
+            <li class="element"><a href="/programs/Php_Projects/Login_System/login.php">Login</a></li>
+            <li class="element"><a href="/programs/Php_Projects/Login_System/signup.php">Sign up</a></li>
         </ul>
     </div>
 </nav>
