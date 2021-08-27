f8e931a9cac03075b8d785b0f87ef8d80cee12c1
Shashwat <harshshashwat18@gmail.com>
2021-08-27 21:50:31 +0530

    Adding basic signupforms

--- signup.php
+++ signup.php
@@ -10,9 +10,23 @@
 <?php require 'partials/nav.php' ?>
 <div class="container" style=" margin: auto;width: 100%;text-align: center;">
     <h1 style="justify-content:center;">Signup Page</h1>
-    <form action="">
-        <label for="uname">User name:</label>
-        <input type="text">
+    <form action="" method="post">
+        <div class="form-grp"></div>
+        <label for="uname">Name</label>
+        <input type="text" name="uname" class="form-elem" id="in1">
+        </div>
+        <div class="form-grp">
+            <label for="email">Email</label>
+            <input type="email" name="email" class="form-elem id="in2">
+        </div>
+        <div class="from-grp">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="in3" class="form-elem">
+        </div>
+        <div class="from-grp">
+            <label for="cpassword">Conform Password</label>
+            <input type="password" name="cpassword" id="in3" class="form-elem">
+        </div>
     </form>
     </div>
 </body>
