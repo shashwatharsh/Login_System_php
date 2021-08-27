e4aa25cd3f4d4edccc8e84b9c60b901245f0073e
Shashwat <harshshashwat18@gmail.com>
2021-08-27 22:36:28 +0530

    Adding css-signup

--- signup.php
+++ signup.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="static/css/sign.css">
     <title>Document</title>
 </head>
 <body>
@@ -11,19 +12,19 @@
 <div class="container" style=" margin: auto;width: 100%;text-align: center;">
     <h1 style="justify-content:center;">Signup Page</h1>
     <form action="" method="post">
-        <div class="form-grp"></div>
+        <div class="form-grp">
         <label for="uname">Name</label>
         <input type="text" name="uname" class="form-elem" id="in1">
         </div>
         <div class="form-grp">
             <label for="email">Email</label>
-            <input type="email" name="email" class="form-elem id="in2">
+            <input type="email" name="email" class="form-elem" id="in2">
         </div>
-        <div class="from-grp">
+        <div class="form-grp">
             <label for="password">Password</label>
             <input type="password" name="password" id="in3" class="form-elem">
         </div>
-        <div class="from-grp">
+        <div class="form-grp">
             <label for="cpassword">Conform Password</label>
             <input type="password" name="cpassword" id="in3" class="form-elem">
         </div>
