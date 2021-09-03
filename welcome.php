bd23977604c3980784e9ed2a1cfaf71a60218d76
Shashwat <harshshashwat18@gmail.com>
2021-09-03 21:45:31 +0530

    updating logout.php

--- welcome.php
+++ welcome.php
@@ -16,8 +16,8 @@ if(!isset($_SESSION['loggedin'])|| $_SESSION['loggedin']!=true){
 <body>
 <?php require 'partials/nav.php' ?>
 <!-- Have to print my name here -->
-    <h1>Pranam <?php echo $_SESSION['name'] ?></h1>
+    <h1>Pranam <?php #echo $_SESSION['name'] ?></h1>
     <p>hi there, shashwat here, I am shashwat harsh and i am hrer to share some insights about my projects and about some programming languages</p>
-    
+    <p>You can logout via this <a href="logout.php">link</a></p>
 </body>
 </html>
