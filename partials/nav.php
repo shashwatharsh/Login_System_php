a089ef9ce6216ed04eae7d73ee13d6287c7dcfb4
Shashwat <harshshashwat18@gmail.com>
2021-09-03 22:06:08 +0530

    updating nav itmes

--- partials/nav.php
+++ partials/nav.php
@@ -2,8 +2,17 @@
 <style>
     <?php require 'static/css/nav.css' ?>
 </style>
+<?php 
+#session_start();
+if(!isset($_SESSION['loggedin'])|| $_SESSION['loggedin']!=true){
+    $loggedin = false;
+}
+else{
+    $loggedin = true;
+}
 
-<nav id='navbar' class="site-handler">
+echo 
+'<nav id="navbar" class="site-handler">
     <div class="nav-container"> 
         <ul class="nav-list">
             <li class="element"><a href="/programs/Php_Projects/Login_System">Home</a></li>
@@ -12,10 +21,17 @@
             <li class="element"><a href="">Contact us</a></li>
             <li class="element"><a href="">About</a></li>
         </ul>
-        <ul class="nav-list log">
-            <li class="element"><a href="/programs/Php_Projects/Login_System/login.php">Login</a></li>
-            <li class="element"><a href="/programs/Php_Projects/Login_System/signup.php">Sign up</a></li>
-            <li class="element"><a href="/programs/Php_Projects/Login_System/logout.php">Logout</a></li>
-        </ul>
+        <ul class="nav-list log">';
+            if (!$loggedin) {
+            echo '<li class="element"><a href="/programs/Php_Projects/Login_System/login.php">Login</a></li>
+            <li class="element"><a href="/programs/Php_Projects/Login_System/signup.php">Sign up</a></li>';
+            }
+            if ($loggedin) {
+            echo '<li class="element"><a href="/programs/Php_Projects/Login_System/logout.php">Logout</a></li>';
+            }
+
+       echo '</ul>
     </div>
-</nav>
+</nav>';
+
+?>
